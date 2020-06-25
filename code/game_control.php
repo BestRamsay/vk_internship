b3e349b57799f7c0407d4125bc40cee5aabcb34c
BestRamsay <a.ta.2000@mail.ru>
2020-06-25 22:18:55 +0300

    Added parser step

--- code/game_control.php
+++ code/game_control.php
@@ -7,6 +7,16 @@ include_once 'defines_army.php';
 define(WHITE, 0);
 define(BLACK, 1);
 
+define(ALIEN_STEP, 1);
+define(MY_STEP, 0);
+
+define(STUB, 0);
+define(FIGURE, 0);
+define(LETTER_START, 1);
+define(DIGIT_START, 2);
+define(LETTER_FINISH, 4);
+define(DIGIT_FINISH, 5);
+define(FIGURE, 0);
 
 interface manage_step {
 
@@ -14,7 +24,6 @@ interface manage_step {
 	public function set_step($figure, $start, $finish);
 
 }
-
 
 class game_control implements manage_step
 {
@@ -27,7 +36,6 @@ class game_control implements manage_step
 	}
 
 }
-
 
 function control_user($hash , $id)
 {
@@ -47,8 +55,7 @@ function control_user($hash , $id)
 			}elseif ($queue % 2 == BLACK && $gamer_2 == $id) {
 				fclose($info);
 				return 1;
-			}
-			
+			}			
 		}
 	}else {
 		return 0;
@@ -95,10 +102,22 @@ function update_board()
 	return read_step(trim(control_valid_key_game($_COOKIE["hash"] , NEED)));
 }
 
-function change_my_board($alien_step)
+function change_my_board($launc_mode, $figure, $start, $finish)
 {
 	/*тут должны код для редактирования "доски"
 	но пока сама доска еще не написано, так что будет стоять заглушка с echo "$alien_step".NEW_STR*/
+
+	switch ($launc_mode) {
+		case ALIEN_STEP:
+			$change = parser_step(read_step(trim(control_valid_key_game($_COOKIE["hash"] , NEED))));
+			break;
+		
+		case MY_STEP:
+			$string_step = $figure.$start."-".$finish;
+			$change = parser_step($string_step);
+			break;
+
+	}
 
 	echo "$alien_step".NEW_STR;
 }
@@ -106,7 +125,6 @@ function change_my_board($alien_step)
 
 function make_step($figure, $point_A , $point_B)
 {
-
 	if (control_user($_COOKIE["hash"], $_COOKIE["id"]))
 	{
 		if(!isset($board)){
@@ -115,11 +133,12 @@ function make_step($figure, $point_A , $point_B)
 			echo "create new board".NEW_STR;
 		}else
 			echo "board exist".NEW_STR;
-		
-		change_my_board(trim(update_board()));
+
+		change_my_board(ALIEN_STEP, STUB, STUB, STUB);
 		if (all_control($figure, $point_A , $point_B))
 		{
-			write_step(trim(control_valid_key_game($_COOKIE["hash"] , NEED)), "$figure"."$point_A"."-"."$point_B"."\n");
+			change_my_board(MY_STEP, $figure, $point_A , $point_B);
+			write_step(trim(control_valid_key_game($_COOKIE["hash"] , NEED)), $figure.$point_A."-".$point_B."\n");
 			echo "all correct fig: ".$figure." A: ".$point_A." B: ".$point_B.NEW_STR;
 		}else
 		echo "Incorrect step".NEW_STR;
@@ -127,18 +146,125 @@ function make_step($figure, $point_A , $point_B)
 	echo "You can't make a move".NEW_STR;
 }
 
-function parser_step($step , $parse_step)
+function parser_step($parse_string)
 {
-	$parse_step = array("figure" => 0, "letter" => 0 , "numeral" =>0);
-	$parse_step["figure"] = $step;
+	$info = array("figure" => "0", "start" => array(0 , 0) , "finish" =>array(0 , 0));
 	
-	return $parse_step;
+	//parse figure
+	switch ($parse_string[FIGURE]) {
+		case KING:
+			$info["figure"][0] = KING;		
+			break;
+		case QUEEN:
+			$info["figure"][0] = QUEEN;
+			break;
+		case BISHOP:
+			$info["figure"][0] = BISHOP;
+			break;
+		case CASTLE:
+			$info["figure"][0] = CASTLE;
+			break;
+		case KNIGHT:
+			$info["figure"][0] = KNIGHT;
+			break;
+		case PAWN:
+			$info["figure"][0] = PAWN;
+			break;
+		
+		default:
+			echo "Invalid input figure".NEW_STR;
+			return 0;
+			break;
+	}
+
+	//parse letter start
+	switch ($parse_string[LETTER_START]) {
+		case "a":
+			$info["start"][0] = 1;
+			break;
+		case "b":
+			$info["start"][0] = 2;
+			break;
+		case "c":
+			$info["start"][0] = 3;
+			break;
+		case "d":
+			$info["start"][0] = 4;
+			break;
+		case "e":
+			$info["start"][0] = 5;
+			break;
+		case "f":
+			$info["start"][0] = 6;
+			break;
+		case "g":
+			$info["start"][0] = 7;
+			break;
+		case "h":
+			$info["start"][0] = 8;
+			break;
+
+		default:
+			echo "Invalid input  letter".NEW_STR;
+			return 0;
+			break;
+	}
+
+	if (0 < $parse_string[DIGIT_START] && $parse_string[DIGIT_START] < 9) {
+		$info["start"][1] = $parse_string[DIGIT_START];
+	}else{
+		echo "Invalid input digit".NEW_STR;
+		return 0;
+	}
+
+	//parse letter finish
+	switch ($parse_string[LETTER_FINISH]) {
+		case "a":
+			$info["finish"][0] = 1;
+			break;
+		case "b":
+			$info["finish"][0] = 2;
+			break;
+		case "c":
+			$info["finish"][0] = 3;
+			break;
+		case "d":
+			$info["finish"][0] = 4;
+			break;
+		case "e":
+			$info["finish"][0] = 5;
+			break;
+		case "f":
+			$info["finish"][0] = 6;
+			break;
+		case "g":
+			$info["finish"][0] = 7;
+			break;
+		case "h":
+			$info["finish"][0] = 8;
+			break;
+
+		default:
+			echo "Invalid input letter".NEW_STR;
+			return 0;
+			break;
+	}
+
+	if ( 0 < $parse_string[DIGIT_FINISH] && $parse_string[DIGIT_FINISH] < 9 ) {
+		$info["finish"][1] = $parse_string[DIGIT_FINISH];
+	}else{
+		echo "Invalid input digit".NEW_STR;
+		return 0;
+	}
+
+	return $info;
 }
 
 function make_board()
 {
 	return construct_board();
 }
+
 
 
 ?>
